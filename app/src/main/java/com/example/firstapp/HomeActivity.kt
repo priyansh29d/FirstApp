9eb29ec1a63a2a9c2f0fc164f718e6192174261f
priyansh29d <priyash929@gmail.com>
2022-01-27 14:49:39 +0530

    Coonecting With other application

--- app/src/main/java/com/example/firstapp/HomeActivity.kt
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.example.firstapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -16,11 +17,22 @@ class HomeActivity : AppCompatActivity() {
         tvHome.text = name
 
     }
-
     fun handleClick(view: android.view.View) {
+        when(view.id){
+            R.id.btnSend -> {  startDialer() }
+            R.id.btnMain -> {startMain() }
+        }
+    }
+
+    private fun startDialer() {
+        var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:21345678"))
+        startActivity(dialIntent)
+    }
+
+    private fun startMain() {
         var intent: Intent
-        intent = Intent(this,MainActivity::class.java)
-        intent.putExtra("PJ","Priyansh Jain")
+        intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("PJ", "Priyansh Jain")
         startActivity(intent)
     }
 }
