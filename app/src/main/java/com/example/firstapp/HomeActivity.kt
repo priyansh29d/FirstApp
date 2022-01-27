d5eb1dcc7cf1a583d34a6ef9a569c9bd6c0c3b8c
priyansh29d <priyash929@gmail.com>
2022-01-27 11:59:55 +0530

    Kotlin

---
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    fun handleClick(view: android.view.View) {
+        Toast.makeText(this,"sending",Toast.LENGTH_SHORT).show()
+    }
+}
