397844d322935096da41cd2ff836085b35fad7ac
priyansh29d <priyash929@gmail.com>
2022-01-27 12:34:37 +0530

    Pass Data

--- app/src/main/java/com/example/firstapp/HomeActivity.kt
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -3,17 +3,24 @@ package com.example.firstapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var name = intent.extras?.getString("sk")
+        var tvHome = findViewById<TextView>(R.id.tvHome)
+        tvHome.text = name
+
     }
 
     fun handleClick(view: android.view.View) {
         var intent: Intent
         intent = Intent(this,MainActivity::class.java)
+        intent.putExtra("PJ","Priyansh Jain")
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String name = getIntent().getExtras().getString("ds");
+        TextView mTextView = findViewById(R.id.tvMain);
+        mTextView.setText(name);
     }
 
     public void clickHandler(View view) {
         Toast.makeText(this, "loggin in", Toast.LENGTH_SHORT).show();
         Intent mIntent;
         mIntent  = new Intent(MainActivity.this,HomeActivity.class);
+        mIntent.putExtra("AV","Avichal Pathak");
         startActivity(mIntent);
 
     }
