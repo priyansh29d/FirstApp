73b58c7f7cd53a741e7dc6bac8ecd13cb2bd0374
priyansh29d <priyash929@gmail.com>
2022-01-28 10:36:09 +0530

    Recycle View All parts

--- app/src/main/java/com/example/firstapp/RecyclerActivity.java
+++ app/src/main/java/com/example/firstapp/RecyclerActivity.java
@@ -1,14 +1,23 @@
 package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class RecyclerActivity extends AppCompatActivity {
 
+    String[] countries = new String[]{"Russia","Ukraine","UK","Germany","US","China"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+        RecyclerView countriesRecyclerView = findViewById(R.id.countriesRview);
+        CountriesAdapter adapter = new CountriesAdapter(countries);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        countriesRecyclerView.setLayoutManager(layoutManager);
+        countriesRecyclerView.setAdapter(adapter);
     }
 }
