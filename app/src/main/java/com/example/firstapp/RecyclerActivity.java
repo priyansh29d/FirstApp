065e7e0b3814c891e70346663009f0c6e365aa41
priyansh29d <priyash929@gmail.com>
2022-01-28 11:52:46 +0530

    Activity Lifecycle

--- app/src/main/java/com/example/firstapp/RecyclerActivity.java
+++ app/src/main/java/com/example/firstapp/RecyclerActivity.java
@@ -5,19 +5,59 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class RecyclerActivity extends AppCompatActivity {
+    public  static String TAG = RecyclerActivity.class.getSimpleName();
+    String[] countries = new String[]{"Russia","Ukraine","UK","Germany","US","China"}; //data = plug
 
-    String[] countries = new String[]{"Russia","Ukraine","UK","Germany","US","China"};
-
-    @Override
+    @Override //memory is getting allocated in RAM
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler);
-        RecyclerView countriesRecyclerView = findViewById(R.id.countriesRview);
+
+        Log.i(TAG,"creating");
+
+        setContentView(R.layout.activity_recycler);   //inflating activity_recyclerview layoutinflater
+        RecyclerView countriesRecyclerView = findViewById(R.id.countriesRview); //socket
         CountriesAdapter adapter = new CountriesAdapter(countries);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         countriesRecyclerView.setLayoutManager(layoutManager);
         countriesRecyclerView.setAdapter(adapter);
+    }
+
+    @Override //the actvity is visible to the user
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG,"starting");
+
+    }
+
+    @Override  //activity comes to the foreground--waking up
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG,"resuming -- restore the state of the game");
+
+    }
+
+    // state means the values of the variable -- name of player, score, level, etc
+    @Override //activity goes into the background -- about to sleep
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"pausing -- save the state of the game");
+
+    }
+
+    @Override //activity is moved out of the ram into the hdd -- hibernate windows
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG,"stop");
+
+    }
+
+    @Override //activity doesn't exist in ram/storage
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"destroy");
+
     }
 }
